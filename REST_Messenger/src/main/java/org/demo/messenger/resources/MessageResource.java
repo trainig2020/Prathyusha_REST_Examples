b53d517ad3d8030d4a1af54ce1504a1728f78f8f
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-11 19:49:24 +0530

    Add files via upload

---
+++ REST_Messenger/src/main/java/org/demo/messenger/resources/MessageResource.java
@@ -0,0 +1,31 @@
+package org.demo.messenger.resources;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.demo.messenger.model.Message;
+import org.demo.messenger.service.MessageService;
+
+@Path("/messages")
+public class MessageResource {
+
+	MessageService messageService = new MessageService();
+	
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Message> getMessages()
+	{
+		return messageService.getAllMessages();
+	}
+	
+	/*
+	 * public String getMessage() { return "Hello World";
+	 * 
+	 * }
+	 */
+	
+}
